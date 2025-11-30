c5947fd9ea6ff72c5b43bac28099d25be4b34606
igorpezzinb@gmail.com <igorpezzinb@gmail.com>
2025-11-30 20:05:41 -0300

    Vamos, Pixies; feito

--- 03_bibliotecas/bibli_04/Resultados/Igor/main/main.c
+++ 03_bibliotecas/bibli_04/Resultados/Igor/main/main.c
@@ -1,1 +1,35 @@
 #include <stdio.h>
+#include "data.h"
+
+int main(){
+    int dia, mes, ano;
+
+    scanf("%d/%d/%d", &dia, &mes, &ano);
+
+    if(verificaDataValida(dia, mes, ano)){
+
+        printf("Data informada: ");
+        imprimeDataExtenso(dia, mes, ano);     
+
+        if(verificaBissexto(ano)){
+            printf("O ano informado eh bissexto\n");
+
+        }else{
+            printf("O ano informado nao eh bissexto\n");
+
+        }
+        
+        printf("O mes informado possui %02d dias\n", numeroDiasMes(mes, ano));
+
+        printf("A data seguinte eh: ");
+
+        imprimeProximaData(dia, mes, ano);
+
+
+    }else{
+        printf("A data informada eh invalida\n");
+    }
+
+
+    return 0;
+}
